4d68c4fea0371cd8d4480aa293a681afc36cf56f
SumeetHaryani <sumeetharyani3@gmail.com>
2020-03-29 19:13:11 +0530

    exercise 2.11. soultion added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Person from './components/Person';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
+
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setNewFilter] = useState('');
-
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data);
+    });
+  }, []);
   const checkUniqueName = () => {
     const arr = persons.filter(person => person.name === newName);
     return arr.length > 0 ? false : true;
