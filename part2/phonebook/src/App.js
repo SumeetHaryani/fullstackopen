cc0a7758f1f10ddb8dc45c8caa29476625bc6b72
SumeetHaryani <sumeetharyani3@gmail.com>
2020-04-07 17:12:58 +0530

    exercises 2.15.-2.18 solutions

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,51 +2,77 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Person from './components/Person';
 import PersonForm from './components/PersonForm';
-import axios from 'axios';
+import phoneService from './services/phone';
+import phone from './services/phone';
 
 function App() {
-  const [persons, setPersons] = useState([
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setNewFilter] = useState('');
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      setPersons(response.data);
+    phoneService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
   const checkUniqueName = () => {
-    const arr = persons.filter(person => person.name === newName);
-    return arr.length > 0 ? false : true;
+    const arr = persons.filter((person) => person.name === newName);
+    return { unique: arr.length > 0 ? false : true, obj: arr[0] };
   };
 
-  const addPerson = event => {
+  const addPerson = (event) => {
     event.preventDefault();
-    if (!checkUniqueName() || newName.length === 0) {
-      alert(`${newName} is already added to phonebook`);
+    const res = checkUniqueName();
+    if (!res.unique || newName.length === 0) {
+      const result = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
+      );
+      if (result) {
+        const personObject = { ...res.obj, number: newNumber };
+        const id = personObject.id;
+        phoneService.update(id, personObject).then((returnedPerson) => {
+          setPersons(
+            persons.map((person) =>
+              person.id !== id ? person : returnedPerson
+            )
+          );
+        });
+      }
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName('');
-      setNewNumber('');
+      const personObject = { name: newName, number: newNumber };
+      phoneService.create(personObject).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName('');
+        setNewNumber('');
+      });
     }
   };
-
-  const handleNameChange = event => {
+  
+  const deletePerson = (id, name) => {
+    //console.log("deletePerson -> id", id)
+    const result = window.confirm(`Delete ${name} ?`);
+    if (result) {
+      phoneService
+        .deletePerson(id)
+        .then(() => setPersons(persons.filter((person) => person.id != id)));
+    }
+  };
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
-  const handleNumberChange = event => {
+  const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
 
-  const handleFilterChange = event => {
+  const handleFilterChange = (event) => {
     setNewFilter(event.target.value);
   };
   const personsListToShow =
     filter === ''
       ? persons
-      : persons.filter(person =>
+      : persons.filter((person) =>
           person.name.toLowerCase().includes(filter.toLowerCase())
         );
   return (
@@ -60,7 +86,7 @@ function App() {
         number={{ value: newNumber, onChange: handleNumberChange }}
       />
       <h2>Numbers</h2>
-      <Person persons={personsListToShow} />
+      <Person persons={personsListToShow} handleDelete={deletePerson} />
     </div>
   );
 }
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Person = ({ persons }) => {
-  return persons.map(person => (
+const Person = (props) => {
+  console.log("Person -> props", props)
+  return props.persons.map(person => (
     <p key={person.name}>
       {person.name}&nbsp;
       {person.number}
+      <button onClick={()=>props.handleDelete(person.id,person.name)}>delete</button>
     </p>
   ));
 };
